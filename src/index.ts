a72029fdf660a3c05a1c8c4b24ac279c0d21e4aa
sajdkabir <sajda.kbir@gmail.com>
2024-12-06 14:13:42 +0530

    prisma

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { Elysia } from "elysia";
+import { swagger } from "@elysiajs/swagger";
 
 const app = new Elysia()
-.get("/", () => "Hello Elysia")
-.get("/hey.", () => ({ message: "hmm....working" }))
-.listen(3000);
-
+    // Apply the swagger plugin
+    .use(swagger()) 
+    .get('/', ({ path }) => path)
+    .post('/hello', 'Do you miss me?')
+    .listen(3000)
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
